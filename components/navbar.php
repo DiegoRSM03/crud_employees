f782e8f107ea475fcd3f2b2181390769b5ffe30f
diego <diegorsm03@gmail.com>
2020-07-02 14:08:05 -0300

    Estilos actualizados

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <nav class="container">
 	<div class="content">
 		<div class="title">Employees Database</div>
-		<span class="flaticon-list"></span>
+		<!-- <span class="flaticon-list"></span> -->
 	</div>
 </nav>
